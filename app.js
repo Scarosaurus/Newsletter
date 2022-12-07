46187d94f060c6de9b1485a198811d48af8133d1
Axolotech <hola.axolotech@gmail.com>
2022-12-07 16:41:04 -0500

    Regret

--- app.js
+++ app.js
@@ -1,68 +1,8 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-const https = require("https")
-
 const app = express()
 
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended: true}))
+const port = process.env.PORT || 3000
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signup.html")
-})
+app.listen(port)
 
-app.post("/", function (req, res) {
-  const firstName = req.body.firstName
-  const lastName = req.body.lastName
-  const email = req.body.email
-
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName
-        }
-      }
-    ]
-  }
-  
-  const jsonData = JSON.stringify(data)
-  
-  const apiKey = "41b9a9a0d5129e3a7330a3ef42c57273-us21"
-  const apiServer = "us21"
-  const listID = "d9ed77d558"
-  
-  const url = "https://" + apiServer + ".api.mailchimp.com/3.0/lists/" + listID
-
-  const options = {
-    method: "POST",
-    auth: "scarosaurus:" + apiKey
-  }
-
-  const request = https.request(url, options, function (response) {
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html")
-    } else {
-      res.sendFile(__dirname + "/failure.html")
-    }
-
-    response.on("data", function (data) {
-      console.log(JSON.parse(data))
-    })
-  })
-
-  request.write(jsonData)
-  request.end()
-})
-
-app.post("/failure", function (req, res) {
-  res.redirect("/")
-})
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port 300");  
-})
-
+console.log("Listen!")
